81e014cd9661dabe71e3628ebc7f0d1806b72ad5
Sergio Martí <smarti@uji.es>
2022-02-27 20:08:50 +0100

    ...

---
+++ qm3/dftd4.f90
@@ -0,0 +1,65 @@
+module qm3
+    use dftd4, only: structure_type, new, d4_model, new_d4_model, rational_damping_param, get_dispersion, realspace_cutoff
+    implicit none
+    public
+    real*8, parameter  :: AA__Bohr = 1.0d0 / 0.52917726d0
+    real*8, parameter  :: AA__Ener = 2625.49964037578d0
+    type( structure_type ) :: mol
+    type( d4_model ) :: d4
+    type( rational_damping_param ) :: param
+end module qm3
+
+
+subroutine qm3_dftd4_init( nat, siz, dat )
+	use qm3
+	implicit none
+	integer, intent( in ) :: nat, siz
+	real*8, dimension(0:siz-1), intent( inout ) :: dat
+    real*8, dimension(:,:), allocatable :: xyz
+    integer, dimension(:), allocatable :: atn
+    integer :: i
+
+    allocate( atn(1:nat), xyz(1:3,1:nat) )
+    do i = 1, nat
+        atn(i) = dint( dat(i) )
+    end do
+    xyz = 0.0d0
+    call new( mol, atn, xyz )
+    mol%charge = dat(0)
+    call new_d4_model( d4, mol, ga = 3.0d0, gc = 2.0d0, wf = 6.0d0 )
+    param = rational_damping_param( s6 = dat(nat+1), s8 = dat(nat+2), s9 = 1.0d0, a1 = dat(nat+3), a2 = dat(nat+4), alp = 16.0d0 )
+    deallocate( atn, xyz )
+end subroutine qm3_dftd4_init
+
+
+subroutine qm3_dftd4_calc( nat, siz, dat )
+	use qm3
+	implicit none
+	integer, intent( in ) :: nat, siz
+	real*8, dimension(0:siz-1), intent( inout ) :: dat
+    real*8, dimension(:,:), allocatable :: grad, sigm
+    real*8 :: ener
+	integer :: i, j, n
+
+	do i = 1, nat
+		j = 3 * ( i - 1 )
+		mol%xyz(1,i) = dat(j  ) * AA__Bohr
+		mol%xyz(2,i) = dat(j+1) * AA__Bohr
+		mol%xyz(3,i) = dat(j+2) * AA__Bohr
+	end do
+
+    allocate( grad(1:3,1:nat), sigm(1:3,1:3) )
+    call get_dispersion( mol, d4, param, realspace_cutoff(), ener, grad, sigm )
+
+	dat(0) = ener * AA__Ener
+    grad = grad * AA__Ener * AA__Bohr
+	do i = 1, nat
+		j = 3 * ( i - 1 )
+		dat(j+1) = grad(1,i)
+		dat(j+2) = grad(2,i)
+		dat(j+3) = grad(3,i)
+	end do
+
+    deallocate( grad, sigm )
+
+end subroutine qm3_dftd4_calc
--- qm3/engines.py
+++ qm3/engines.py
@@ -470,6 +470,83 @@ class qm3_dftb( object ):
 # =================================================================================================
 # dftd4
 
+class qm3_dftd4( object ):
+    def __init__( self, mol: object, parm: dict,
+            sel_QM: typing.Optional[numpy.array] = numpy.array( [], dtype=numpy.bool ),
+            link: typing.Optional[list] = [] ):
+        if( sel_QM.sum() > 0 ):
+            self.sel = numpy.argwhere( sel_QM ).ravel()
+        else:
+            self.sel = numpy.arange( mol.natm )
+        self.lnk = link[:]
+        self.vla = []
+        self.nat = len( self.sel )
+        self.all = self.nat + len( self.lnk )
+        self.siz = max( 6, self.all * 3 + 1 )
+        self.vec = ( ctypes.c_double * self.siz )()
+        cwd = os.path.abspath( os.path.dirname( inspect.getfile( self.__class__ ) ) ) + os.sep
+        self.lib = ctypes.CDLL( cwd + "_dftd4.so" )
+        self.lib.qm3_dftd4_init_.argtypes = [
+                ctypes.POINTER( ctypes.c_int ),
+                ctypes.POINTER( ctypes.c_int ),
+                ctypes.POINTER( ctypes.c_double ) ]
+        self.lib.qm3_dftd4_init_.restype = None
+        self.lib.qm3_dftd4_calc_.argtypes = [
+                ctypes.POINTER( ctypes.c_int ),
+                ctypes.POINTER( ctypes.c_int ),
+                ctypes.POINTER( ctypes.c_double ) ]
+        self.lib.qm3_dftd4_calc_.restype = None
+        self.vec[0] = parm["chrg"]
+        k = 1
+        for i in self.sel:
+            self.vec[k] = mol.anum[i]
+            k += 1
+        if( len( self.lnk ) > 0 ):
+            for i in range( len( self.lnk ) ):
+                self.vec[k] = 1
+                k += 1
+        self.vec[self.all+1] = parm["s6"]
+        self.vec[self.all+2] = parm["s8"]
+        self.vec[self.all+3] = parm["a1"]
+        self.vec[self.all+4] = parm["a2"]
+        self.lib.qm3_dftd4_init_( ctypes.c_int( self.all ), ctypes.c_int( self.siz ), self.vec )
+
+
+    def update_coor( self, mol ):
+        l = 0
+        for i in self.sel:
+            for j in [0, 1, 2]:
+                self.vec[l] = mol.coor[i,j]
+                l += 1
+        self.vla = []
+        k = len( self.sel )
+        for i in range( len( self.lnk ) ):
+            c, v = Link_coor( self.lnk[i][0], self.lnk[i][1], mol )
+            for j in [0, 1, 2]:
+                self.vec[l] = c[j]
+                l += 1
+            self.vla.append( ( self.sel.searchsorted( self.lnk[i][0] ), k, v ) )
+            k += 1
+
+
+    def get_func( self, mol, density = False ):
+        self.update_coor( mol )
+        self.lib.qm3_dftd4_calc_( ctypes.c_int( self.all ), ctypes.c_int( self.siz ), self.vec )
+        mol.func += self.vec[0]
+
+
+    def get_grad( self, mol ):
+        self.update_coor( mol )
+        self.lib.qm3_dftd4_calc_( ctypes.c_int( self.all ), ctypes.c_int( self.siz ), self.vec )
+        mol.func += self.vec[0]
+        g = [ j for j in self.vec[1:] ]
+        Link_grad( self.vla, g )
+        l = 0
+        for i in self.sel:
+            for j in [0, 1, 2]:
+                mol.grad[i,j] += g[l]
+                l += 1
+
 # =================================================================================================
 
 try:
