f6d8633ec17a9c54161d6b93cd207a69967b48d0
Sergio Martí <smarti@uji.es>
2022-12-19 18:28:06 +0100

    ...

--- tools/rmsd.py
+++ tools/rmsd.py
@@ -2,20 +2,26 @@
 import  sys
 import  numpy
 import  qm3
+import  qm3.utils._dcd
+import  matplotlib.pyplot as plt
 
 mol = qm3.molecule()
-mol.pdb_read( sys.argv[1] )
+mol.prmtop_read( open( "../just_protein.prmtop" ) )
 
 sel = mol.labl == "C"
-sel = numpy.logical_or( mol.labl == "N" )
-sel = numpy.logical_or( mol.labl == "CA" )
+sel = numpy.logical_or( sel, mol.labl == "N" )
+sel = numpy.logical_or( sel, mol.labl == "CA" )
+
+sel = mol.anum > 1
+
+dcd = qm3.utils._dcd.dcd()
+dcd.open_read( "just_protein.dcd" )
+dcd.next( mol )
 
 ref = mol.coor[sel]
 ref -= numpy.average( ref, axis = 0 )
 
-dcd = qm3.fio.dcd.dcd()
-dcd.open_read( sys.argv[2] )
-
+out = []
 while( dcd.next( mol ) ):
     cur = mol.coor[sel]
     cur -= numpy.average( cur, axis = 0 )
@@ -24,6 +30,11 @@ while( dcd.next( mol ) ):
     if( numpy.linalg.det( cov ) < 0 ):
         r2[2,:] *= -1.0
     mod = numpy.dot( cur, numpy.dot( r1, r2 ) )
-    print( numpy.sqrt( numpy.average( numpy.sum( ( ref - mod ) ** 2, axis = 1 ) ) ) )
-
+    out.append( numpy.sqrt( numpy.average( numpy.sum( ( ref - mod ) ** 2, axis = 1 ) ) ) )
 dcd.close()
+out = numpy.array( out )
+plt.clf()
+plt.grid( True )
+plt.plot( out, '-' )
+plt.savefig( "rmsd.pdf" )
+plt.show()
