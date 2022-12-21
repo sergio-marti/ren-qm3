1f704ff24f96cad5cde322cb8a779e0c9eff64c9
Sergio Martí <smarti@uji.es>
2022-12-21 09:23:17 +0100

    ...

--- tools/bbone_anal.py
+++ tools/bbone_anal.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import  numpy
 import  qm3
-import  qm3.data
 import  qm3.utils._dcd
 import  matplotlib.pyplot as plt
 import  matplotlib.backends.backend_pdf
@@ -10,10 +9,12 @@ import  sklearn.decomposition
 mol = qm3.molecule()
 mol.prmtop_read( open( "prmtop" ) )
 
-sel = mol.labl == "CA"
-sel = numpy.logical_or( sel, mol.labl == "C" )
+idx = mol.labl == "CA"
+sel = numpy.logical_or( idx, mol.labl == "C" )
 sel = numpy.logical_or( sel, mol.labl == "N" )
+
 sel = numpy.flatnonzero( sel )
+idx = numpy.flatnonzero( numpy.in1d( sel, numpy.flatnonzero( idx ) ) )
 
 dcd = qm3.utils._dcd.dcd()
 dcd.open_read( "dcd" )
@@ -22,6 +23,8 @@ dcd.next( mol )
 ref = mol.coor[sel]
 ref -= numpy.average( ref, axis = 0 )
 
+out = []
+dif = []
 crd = []
 while( dcd.next( mol ) ):
     cur = mol.coor[sel]
@@ -30,18 +33,61 @@ while( dcd.next( mol ) ):
     r1, s, r2 = numpy.linalg.svd( cov )
     if( numpy.linalg.det( cov ) < 0 ):
         r2[2,:] *= -1.0
-    crd.append( numpy.dot( cur, numpy.dot( r1, r2 ) ) )
+    mod = numpy.dot( cur, numpy.dot( r1, r2 ) )
+    crd.append( mod )
+    dif.append( mod[idx] - ref[idx] )
+    out.append( numpy.sqrt( numpy.mean( numpy.sum( numpy.square( ref - mod ), axis = 1 ) ) ) )
 dcd.close()
+
+out = numpy.array( out )
+numpy.savetxt( "rmsd.dat", out )
+plt.clf()
+plt.grid( True )
+plt.plot( out, '-' )
+plt.xlabel( "Time" )
+plt.ylabel( "RMSd $(N,C_{\\alpha},C)$" )
+plt.tight_layout()
+plt.savefig( "rmsd.pdf" )
+plt.show()
+
+med = numpy.mean( dif, axis = 0 )
+out = numpy.sqrt( numpy.mean( numpy.sum( numpy.square( dif - med ), axis = 2 ), axis = 0 ) )
+numpy.savetxt( "rmsf.dat", out )
+plt.clf()
+plt.grid( True )
+plt.plot( out, '-' )
+plt.xlabel( "Residue" )
+plt.ylabel( "RMSf $(C_{\\alpha})$" )
+plt.tight_layout()
+plt.savefig( "rmsf.pdf" )
+plt.show()
+
+dif = numpy.array( dif )
+dr2 = numpy.sqrt( numpy.mean( numpy.sum( numpy.square( dif ), axis = 2 ), axis = 0 ) )
+img = numpy.zeros( ( dif.shape[1], dif.shape[1] ) )
+with open( "dccm.dat", "wt" ) as f:
+    for i in range( dif.shape[1] ):
+        for j in range( dif.shape[1] ):
+            img[i,j] = numpy.sum( dif[:,i,:] * dif[:,j,:] ) / ( dif.shape[0] * dr2[i] * dr2[j] )
+            f.write( "%10d%10d%20.10lf\n"%( i+1, j+1, img[i,j] ) )
+        f.write( "\n" )
+plt.clf()
+plt.grid( True )
+plt.imshow( img, cmap = "coolwarm" )
+plt.colorbar()
+plt.xlabel( "Residue $(C_{\\alpha})$" )
+plt.ylabel( "Residue $(C_{\\alpha})$" )
+plt.tight_layout()
+plt.savefig( "dccm.pdf" )
+plt.show()
+
 crd = numpy.array( crd )
 crd.shape = ( crd.shape[0], crd.shape[1] * crd.shape[2] )
-print( crd.shape )
-
 pca = sklearn.decomposition.PCA( n_components = 2 )
 red = pca.fit_transform( crd )
 with open( "pca.dat", "wt" ) as f:
     for i in range( crd.shape[0] ):
         f.write( "%12.6lf%12.6lf\n"%( red[i,0], red[i,1] ) )
-
 val, vec = numpy.linalg.eigh( pca.get_covariance() )
 print( val[-2:] )
 lbl = qm3.data.symbol[mol.anum[sel]]
@@ -50,15 +96,13 @@ for k in [1, 2]:
         mod = vec[:,-k].reshape( ( lbl.shape[0], 3 ) )
         for i in range( 28 ):
             f.write( "%d\n\n"%( lbl.shape[0] ) )
-            # empirically determined... :d
+            # empirical factor... :d
             fac = 8.5 * numpy.sin( numpy.pi * float( i ) / 14 )
             for j in range( lbl.shape[0] ):
                 f.write( "%-2s%20.10lf%20.10lf%20.10lf\n"%( lbl[j],
                     ref[j,0] + fac * mod[j,0],
                     ref[j,1] + fac * mod[j,1],
                     ref[j,2] + fac * mod[j,2] ) )
-
-
 pdf = matplotlib.backends.backend_pdf.PdfPages( "pca.pdf" )
 plt.clf()
 plt.grid( True )
@@ -73,7 +117,6 @@ plt.legend( loc = "upper right", fontsize = "small" )
 plt.tight_layout()
 pdf.savefig()
 plt.show()
-
 plt.clf()
 plt.grid( True )
 plt.scatter( red[:,0], red[:,1], marker = "o", c = numpy.arange( crd.shape[0] ) )
