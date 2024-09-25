7c401381b36b099d0afe3ab460ee2a65809b3e33
Sergio Martí <smarti@uji.es>
2024-09-25 13:04:23 +0200

    ...

---
+++ qm3/engines/psi4.py
@@ -0,0 +1,111 @@
+import  numpy
+import  typing
+import  os
+import  glob
+import  qm3.data
+import  qm3.engines
+import  psi4
+
+#
+# Set environment variable: PSI_SCRATCH
+#
+
+class run( qm3.engines.template ):
+    def __init__( self, mol: object, 
+            opts: typing.Optional[dict] = { "reference": "rks", "basis": "def2-svp", "d_convergence": 6, "scf_type": "direct",
+                "guess": "read", "output": False, "charge": 0, "method": "b3lyp", "ncpus": 2, "memory": "4096 MB" },
+            sel_QM: typing.Optional[numpy.array] = numpy.array( [], dtype=numpy.bool_ ),
+            sel_MM: typing.Optional[numpy.array] = numpy.array( [], dtype=numpy.bool_ ),
+            link: typing.Optional[list] = [] ):
+        qm3.engines.template.__init__( self, mol, sel_QM, sel_MM, link )
+        self.cx  = 1.0 / qm3.data.A0
+        self.ce  = qm3.data.H2J
+        self.cg  = self.ce * self.cx
+
+        buf = "\n%d 1\n"%( opts.pop( "charge" ) )
+        for i in self.sel:
+            buf += "%-2s%20.10lf%20.10lf%20.10lf\n"%( qm3.data.symbol[mol.anum[i]],
+                    mol.coor[i,0], mol.coor[i,1], mol.coor[i,2] )
+        self.vla = []
+        self.__dq = numpy.zeros( mol.natm )
+        if( len( self.lnk ) > 0 ):
+            k = len( self.sel )
+            for i in range( len( self.lnk ) ):
+                c, v = qm3.engines.Link_coor( self.lnk[i][0], self.lnk[i][1], mol )
+                buf += "%-2s%20.10lf%20.10lf%20.10lf\n"%( "H", c[0], c[1], c[2] )
+                self.vla.append( ( self.sel.searchsorted( self.lnk[i][0] ), k, v ) )
+                k += 1
+            # redistribute MM-charge on the remaining atoms of the group
+            for i,j in self.lnk:
+                if( j in self.grp ):
+                    self.__dq[self.grp[j]] += mol.chrg[j] / len( self.grp[j] )
+            # ----------------------------------------------------------
+        if( opts.pop( "output" ) ):
+            psi4.core.set_output_file( "psi4.out", False )
+        else:
+            psi4.core.be_quiet()
+        psi4.set_memory( opts.pop( "memory" ) )
+        psi4.set_num_threads( opts.pop( "ncpus" ) )
+        buf += "symmetry c1\nnoreorient\nnocom\nunits ang\n"
+        self.aQM = psi4.geometry( buf )
+        self.met = opts.pop( "method" )
+        psi4.set_options( opts )
+
+
+    def update_coor( self, mol ):
+        crd = []
+        for i in self.sel:
+            crd.append( ( self.cx * mol.coor[i] ).tolist() )
+        self.vla = []
+        if( len( self.lnk ) > 0 ):
+            k = len( self.sel )
+            for i in range( len( self.lnk ) ):
+                c, v = qm3.engines.Link_coor( self.lnk[i][0], self.lnk[i][1], mol )
+                crd.append( ( self.cx * c ).tolist() )
+                self.vla.append( ( self.sel.searchsorted( self.lnk[i][0] ), k, v ) )
+                k += 1
+        self.aQM.set_geometry( psi4.core.Matrix.from_list( crd ) )
+        self.aQM.update_geometry()
+        self.aMM = None
+        if( len( self.nbn ) > 0 ):
+            self.aMM = []
+            f = open( "grid.dat", "wt" )
+            for i in self.nbn:
+                tmp = mol.coor[i].copy()
+                if( self.img ):
+                    tmp -= mol.boxl * numpy.round( tmp / mol.boxl, 0 )
+                self.aMM.append( [ mol.chrg[i] + self.__dq[i], tmp[0] * self.cx, tmp[1] * self.cx, tmp[2] * self.cx ] )
+                f.write( "%20.10lf%20.10lf%20.10lf\n"%( tmp[0], tmp[1], tmp[2] ) )
+            f.close()
+            self.aMM = numpy.array( self.aMM )
+
+
+    def get_func( self, mol ):
+        self.update_coor( mol )
+        mol.func += psi4.energy( self.met, return_wfn = False, external_potentials = self.aMM ) * self.ce
+
+
+    def get_grad( self, mol ):
+        self.update_coor( mol )
+        g, wfn = psi4.gradient( self.met, return_wfn = True, external_potentials = self.aMM )
+        mol.func += psi4.variable( "CURRENT ENERGY" ) * self.ce
+        g = ( self.cg * g.to_array() ).ravel()
+        qm3.engines.Link_grad( self.vla, g )
+        k = 0
+        for i in self.sel:
+            for j in [0, 1, 2]:
+                mol.grad[i,j] += g[k]
+                k += 1
+        if( len( self.nbn ) > 0 ):
+            ef = psi4.core.OEProp( wfn )
+            ef.add( "GRID_FIELD" )
+            ef.compute()
+            efx = ef.Exvals()
+            efy = ef.Eyvals()
+            efz = ef.Ezvals()
+            k = 0
+            for i in self.nbn:
+                mol.grad[i,0] -= self.cg * ( mol.chrg[i] + self.__dq[i] ) * efx[k]
+                mol.grad[i,1] -= self.cg * ( mol.chrg[i] + self.__dq[i] ) * efy[k]
+                mol.grad[i,2] -= self.cg * ( mol.chrg[i] + self.__dq[i] ) * efz[k]
+                k += 1
---
+++ samples/test_psi4.py
@@ -0,0 +1,45 @@
+import	numpy
+import	qm3
+import  qm3.engines.psi4
+import  os
+import  sys
+
+ 
+cwd = os.path.abspath( os.path.dirname( sys.argv[0] ) ) + os.sep
+
+
+mol = qm3.molecule()
+mol.pdb_read( open( cwd + "charmm.pdb" ) )
+mol.psf_read( open( cwd + "charmm.psf" ) )
+mol.guess_atomic_numbers()
+print( mol.anum )
+print( mol.chrg )
+
+sqm = mol.resn == "WAT"
+for a in [ "C6", "C9", "H11", "H12", "H13", "H14", "H15" ]:
+    sqm[mol.indx["A"][1][a]] = True
+sqm = numpy.logical_not( sqm )
+smm = mol.sph_sel( sqm, 12 )
+#sla = [ ( mol.indx["A"][1]["C10"], mol.indx["A"][1]["C6"] ) ]
+sla = [ ( mol.indx["A"][1]["C10"], mol.indx["A"][1]["C6"], [ mol.indx["A"][1]["H11"], mol.indx["A"][1]["H12"] ] ) ]
+print( sqm.sum(), smm.sum() )
+
+opt = { "reference": "rks",
+       "basis": "def2-svp",
+       "d_convergence": 6,
+       "scf_type": "direct",
+       "guess": "read",
+       "output": True,
+       "charge": 1,
+       "method": "b3lyp",
+       "ncpus": 1,
+       "memory": "4096 MB" }
+mol.engines["qm"] = qm3.engines.psi4.run( mol, opt, sqm, smm, sla )
+
+mol.get_grad()
+#print( round( mol.func, 1 ), "/ -697633.7" )
+#print( round( numpy.linalg.norm( mol.grad ), 1 ), "/ 575.6" )
+#print( round( numpy.linalg.norm( mol.grad[mol.indx["A"][1]["C10"]] ), 1 ), "/ 68.3" )
+print( round( mol.func, 1 ), "/ -697664.9" )
+print( round( numpy.linalg.norm( mol.grad ), 1 ), "/ 571.0" )
+print( round( numpy.linalg.norm( mol.grad[mol.indx["A"][1]["C10"]] ), 1 ), "/ 65.1" )
